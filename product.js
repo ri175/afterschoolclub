5f7e7e149c80b005e0fcc2fd8c5e7ca8be157f4d
ri175 <73915574+ri175@users.noreply.github.com>
2020-11-20 13:43:40 +0000

    Add files via upload
    failed to upload through VSC. Having to upload manually straight from GitHub. Can't seem to find the issue as to why it isn't uploading through the coding directly.

---
+++ product.js
@@ -0,0 +1,92 @@
+let products = [
+    { //ENGLISH SUBJECT
+        id: 1001,
+        subject: "English",
+        location: "London",
+        price: 25,
+        image: "../english.jpg",
+        availableSpaces: 5,
+    },
+    
+
+    { //MATHS SUBJECT
+        id: 1002,
+        subject: "Maths",
+        location: "London",
+        price: 25,
+        image: "../maths.jpg",
+        availableSpaces: 5,
+    },
+
+    { //BIOLOGY SUBJECT
+        id: 1003,
+        subject: "Biology",
+        location: "London",
+        price: 25,
+        image: "../science.jpg",
+        availableSpaces: 5,
+    },
+
+    { //CHEMISTRY SUBJECT
+        id: 1004,
+        subject: "Chemistry",
+        location: "London",
+        price: 20,
+        image: "../science.jpg",
+        availableSpaces: 5,
+    },
+
+    { //PHYSICS SUBJECT
+        id: 1005,
+        subject: "Physics",
+        location: "London",
+        price: 25,
+        image: "../science.jpg",
+        availableSpaces: 5,
+    },
+
+    { //ART SUBJECT
+        id: 1006,
+        subject: "Art",
+        location: "London",
+        price: 20,
+        image: "../art.jpg",
+        availableSpaces: 5,
+    },
+
+    { //FOODTECH SUBJECT
+        id: 1007,
+        subject: "FoodTech",
+        location: "London",
+        price: 20,
+        image: "../foodtech.jpg",
+        availableSpaces: 5,
+    },
+
+    { //HISTORY SUBJECT
+        id: 1008,
+        subject: "History",
+        location: "London",
+        price: 25,
+        image: "../history.jpg",
+        availableSpaces: 5,
+    },
+
+    { //GEOGRAPHY SUBJECT
+        id: 1009,
+        subject: "Geography",
+        location: "London",
+        price: 25,
+        image: "../geography.jpg",
+        availableSpaces: 5,
+    },
+
+    { //SOCIOLOGY SUBJECT
+        id: 1010,
+        subject: "Sociology",
+        location: "London",
+        price: 20,
+        image: "../sociology.jpg",
+        availableSpaces: 5,
+    },
+]
